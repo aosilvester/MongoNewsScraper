c8893998bc988bf4c7a6f5479c794bfade4320a7
Alex <38813232+aosilvester@users.noreply.github.com>
2018-12-09 11:58:05 -0500

    initial commit. starting serverJS

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var handlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
